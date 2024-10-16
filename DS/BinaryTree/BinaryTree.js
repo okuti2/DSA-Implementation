8a604f7ba4a751d4c007dbba316897e10f0f2b3c
Toyosi Kuti <okuti2@myseneca.ca>
2024-10-16 11:17:09 -0400

    UPDATE

---
+++ DS/BinaryTree/BinaryTree.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        const newNode = new Node(data);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+}
+
+function isBinaryTree(root){
+    if(!root){
+        return true;
+    }
+    if(root.left && root.left.data > root.data){
+        return false;
+    }
+    if(root.right && root.right.data < root.data){
+        return false;
+    }
+    return isBinaryTree(root.left) && isBinaryTree(root.right);
+}
